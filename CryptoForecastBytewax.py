cbf8bdec7fb890d57f39435806151d75548d9fd2
lchemaly1 <76957449+lchemaly1@users.noreply.github.com>
2022-07-29 21:52:01 +0300

    Update CryptoForecastBytewax.py

--- CryptoForecastBytewax.py
+++ CryptoForecastBytewax.py
@@ -16,10 +16,10 @@ correctChoice=True
 while correctChoice:
     choice=input("Choose which market you want to forecast \n 1 - BTC/USD \n 2 - ETH/USD \n your pick :  ")
     if int(choice) == 1:
-        cryptoForcast('BTC','BTC/USD', Bytewaxflow, client,  unixStartDate, unixEndDate)
+        cryptoForecast('BTC','BTC/USD', Bytewaxflow, client,  unixStartDate, unixEndDate)
         break;
     elif int(choice)==2:
-        cryptoForcast('ETH','ETH/USD', Bytewaxflow, client,  unixStartDate, unixEndDate)
+        cryptoForecast('ETH','ETH/USD', Bytewaxflow, client,  unixStartDate, unixEndDate)
         break;
     print('Wrong choice, please input either 1 or 2')
     
